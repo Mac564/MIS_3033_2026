ba9f50366f0ec373343461b2d810963596cc9f43
Mac <mclain@southlakegymnastics.com>
2025-09-04 10:04:28 -0500

    Cereal Review

---
+++ Cereal_Problem/Cereal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cereal_Problem
+{
+    public class Cereal
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Calories { get; set; }
+
+        public double Cups { get; set; }
+
+        private double Squirell;
+
+
+        public Cereal()
+        {
+            Manufacturer = "";
+            Name = string.Empty;
+            Calories = 0.0;
+            Cups = 0.0;
+            Squirell = 0.0;
+        }
+
+        public Cereal(string line)
+        {
+            Manufacturer = line;
+            Name = string.Empty;
+            Calories = 0.0;
+            Cups = 0.0; 
+        }
+
+     
+
+    }
+}
---
+++ Cereal_Problem/Program.cs
@@ -0,0 +1,42 @@
+﻿using Cereal_Problem;
+using System.Security;
+
+//Cereal c = new Cereal();
+//c.Manufacturer = "General Mills";
+//c.Name = Console.ReadLine();
+//c.Cups = 5;
+//c.Calories = 28.2;
+
+//Cereal c1 = new Cereal();
+//c.Manufacturer = "General Mills";
+//c.Name = Console.ReadLine();
+//c.Cups = 5;
+//c.Calories = 28.2;
+
+//string fileContents = File.ReadAllText("Cereal_Data.txt");
+
+List <Cereal> allCereals = new List<Cereal>();
+string[] linesOfFile = File.ReadAllLines("Cereal_Data.txt");
+
+for (int i = 1; i < linesOfFile.Length; i++)
+{
+ 
+    //"name | manufacturer | calories | cups"
+    string line = linesOfFile[i];
+    String[] parts = line.Split("|");
+    Cereal c = new Cereal();
+    c.Name = parts[0].Trim();
+    c.Manufacturer = parts[1].Trim();
+    c.Calories = Convert.ToDouble(parts[2].Trim());
+    c.Cups = Convert.ToDouble(parts[3].Trim());
+    allCereals.Add(c);
+}
+
+foreach (var cereal in allCereals)
+{
+    if (cereal.Cups >= 1)
+    {
+        Console.WriteLine(cereal);
+    }
+}
+        
